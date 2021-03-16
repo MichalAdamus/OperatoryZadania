cb6edeafbacdcd4b5124094e1bd539b834aa2265
Michał Adamus <mb.adamus@gmail.com>
2021-03-16 22:48:16 +0100

    #11 Program do wydzielenia reszty z dzielenia

---
+++ App11.java
@@ -0,0 +1,29 @@
+class App11{
+	public static void main(String args[]){
+
+	///////////////// Zadanie 1 /////////////// 
+	int x = 47;
+	int z = 10;
+	int y = 10;
+	int WynikDzielenia;
+	System.out.println();
+	WynikDzielenia = x-(z*y);
+	System.out.println();
+	System.out.println("WynikDzielenia: "+ WynikDzielenia);
+	System.out.println();
+	System.out.println();
+	
+		
+	//////////// Zadanie 2 /////////////// 
+	double n = 35.5;
+	int b = 6;
+	int c = 6;
+	int WynikDzieleniaN;
+	System.out.println();
+	WynikDzieleniaN = (int)n-(b*c);
+	System.out.println();
+	System.out.println("WynikDzieleniaN: "+ WynikDzieleniaN);
+	System.out.println();
+	System.out.println();
+	}
+}
