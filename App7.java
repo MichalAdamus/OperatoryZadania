50219b19d6d522a50db4b5bfc11cc98f4af889d5
Michał Adamus <mb.adamus@gmail.com>
2021-03-16 15:37:25 +0100

    program do oblicznia zmiennych całkowitoliczbowych

---
+++ App7.java
@@ -0,0 +1,15 @@
+class App7{
+
+	public static void main(String args[]){
+			
+		byte zmienna1 = 5;
+        int zmienna2 = 2345;
+        int zmienna3 = (zmienna1 + zmienna2);
+        int zmienna4;
+
+        zmienna3 = zmienna1 + zmienna2;
+        zmienna4 = zmienna1 + zmienna2 + zmienna3;
+
+        System.out.println("zmienna4: "+zmienna4);
+	}
+}
